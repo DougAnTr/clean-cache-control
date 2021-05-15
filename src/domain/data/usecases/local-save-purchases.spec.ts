b32273ea36dbbd3989271f7a0961b4b5c999675b
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-15 10:03:25 -0300

    Ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,11 +1,21 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
-interface CacheStore {}
+interface CacheStore {
+  delete(): void;
+}
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
 describe("LocalSavePurchases", () => {
@@ -14,5 +24,12 @@ describe("LocalSavePurchases", () => {
     const sut = new LocalSavePurchases(cacheStoreSpy);
 
     expect(cacheStoreSpy.deleteCallsCount).toBe(0);
+  });
+
+  it("Should delete old cache on sut.save", async () => {
+    const cacheStoreSpy = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStoreSpy);
+    await sut.save();
+    expect(cacheStoreSpy.deleteCallsCount).toBe(1);
   });
 });
