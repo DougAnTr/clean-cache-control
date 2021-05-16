1fed2386e10d3260eda5fedaa244c26e3f4101a9
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-16 17:24:03 -0300

    Move CacheStoreSpy to its own file

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,38 +1,5 @@
-import { CacheStore } from "@/data/protocols/cache";
-import { mockPurchases } from "@/data/tests/purchases";
-import { SavePurchases } from "@/domain/usecases";
+import { CacheStoreSpy, mockPurchases } from "@/data/tests";
 import { LocalSavePurchases } from "./local-save-purchases";
-
-class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0;
-  insertCallsCount = 0;
-  deleteKey: string;
-  insertKey: string;
-  insertValues: Array<SavePurchases.Params>;
-
-  delete(key: string): void {
-    this.deleteKey = key;
-    this.deleteCallsCount++;
-  }
-
-  insert(key: string, value: any): void {
-    this.insertKey = key;
-    this.insertCallsCount++;
-    this.insertValues = value;
-  }
-
-  simulateDeleteError(): void {
-    jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-
-  simulateInsertError(): void {
-    jest.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-}
 
 type SutTypes = {
   sut: LocalSavePurchases;
