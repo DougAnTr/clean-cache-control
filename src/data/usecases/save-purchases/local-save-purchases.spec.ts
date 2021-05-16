20996b3f2c46f3d8f2fb7345a37b7f02b4d00c55
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-16 16:17:01 -0300

    Ensure LocalSAvePuchases does not insert new cache if delete fails

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -3,6 +3,7 @@ import { LocalSavePurchases } from "./local-save-purchases";
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+  insertCallsCount = 0;
   key: string;
 
   delete(key: string): void {
@@ -39,5 +40,16 @@ describe("LocalSavePurchases", () => {
     await sut.save();
     expect(cacheStore.deleteCallsCount).toBe(1);
     expect(cacheStore.key).toBe("purchases");
+  });
+
+  it("Should not insert new cache if delete fails", () => {
+    const { sut, cacheStore } = makeSut();
+    jest.spyOn(cacheStore, "delete").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.save();
+    expect(promise).rejects.toThrow();
+    expect(cacheStore.insertCallsCount).toBe(0);
   });
 });
