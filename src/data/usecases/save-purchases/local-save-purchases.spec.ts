1cd21b21316db33497d8fb879c251ed2418a9f4a
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-17 05:42:38 -0300

    Ensure LocalSavepurchases resolves successfully

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -39,7 +39,7 @@ describe("LocalSavePurchases", () => {
     const { sut, cacheStore } = makeSut(timestamp);
     const purchases = mockPurchases();
 
-    await sut.save(purchases);
+    const promise = sut.save(purchases);
 
     expect(cacheStore.messages).toEqual([
       CacheStoreSpy.Message.delete,
@@ -51,6 +51,7 @@ describe("LocalSavePurchases", () => {
       timestamp,
       value: purchases,
     });
+    await expect(promise).resolves.toBeFalsy();
   });
 
   it("Should throw if insert throws", async () => {
