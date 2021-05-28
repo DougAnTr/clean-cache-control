4d1e0ec945b59ce43760e28977970d48bfda7c01
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-28 06:42:27 -0300

    Ensure LocalloadPurchases will not delete or insert cache on init

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -0,0 +1,25 @@
+import { CacheStoreSpy, mockPurchases } from "@/data/tests";
+import { LocalLoadPurchases } from "./local-load-purchases";
+
+type SutTypes = {
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (timestamp = new Date()): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timestamp);
+
+  return {
+    sut,
+    cacheStore,
+  };
+};
+
+describe("LocalSavePurchases", () => {
+  it("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+
+    expect(cacheStore.actions).toEqual([]);
+  });
+});
